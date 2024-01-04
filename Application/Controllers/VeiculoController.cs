1ed0c1885f31334d66d0d1ff5937702fcff0d12c
YagoCoelho <yago.scoelho@senacsp.edu.br>
2024-01-03 22:12:58 -0300

    Subindo alterações

---
+++ Application/Controllers/VeiculoController.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Commands;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculoController : ControllerBase
+    {
+        [HttpPost("CadastrarVeiculos")]
+        public IActionResult PostAsync([FromBody] VeiculoCommand command)
+        {
+            return Ok();
+        }
+
+        public IActionResult SimularAluguel() 
+        {
+            return Ok();
+        }
+        public IActionResult Alugar()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Domain/Enums/ETipoVeiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum ETipoVeiculo
+    {
+
+        Ranger = 1,
+        Onix = 2,
+        Model3 = 3,
+        HB20 = 4,
+        Peugeot208 = 5,
+    }
+}
